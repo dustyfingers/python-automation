1469bf8c511bbe7e0066d981662ffc12c62b590b
lou <dustyfingers01257@gmail.com>
2022-01-01 13:43:32 -0700

    start on scraper

---
+++ basic-web-scraping/scraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+
+now = datetime.datetime.now()
+
+content = ''
+
+
+# extracts current news from Hacker News
+def extract_news(url):
+    print("Extracting HackerNews stories...")
+    
+    retval  = '<b>HackerNews Top Stories</b>\n<br>-'*50 + '<br>'
+
+    response = requests.get(url)
+
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+
+    parsed_soup = soup.find_all('td', attrs={'class': 'title', 'valign': ''})
+
+    for i, tag in enumerate(parsed_soup):
+        retval += ((str(i + 1) + ' :: ' + tag.text + '\n<br>') if tag.text != 'More' else '')
+    
+    return retval
